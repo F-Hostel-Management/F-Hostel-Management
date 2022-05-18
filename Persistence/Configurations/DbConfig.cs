2b53a7cb4bc27ff3097ef00a5462a041417c78a6
HuyB <huybui479@gmail.com>
2022-05-18 16:22:25 +0700

    Create strongly type for db conn string

---
+++ Persistence/Configurations/DbConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Configurations
+{
+    public class DbConfig
+    {
+        public string DefaultConnection { get; set; }
+    }
+}
