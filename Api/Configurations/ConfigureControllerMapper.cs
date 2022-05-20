0ed7ec572493c3e6551de7333b3456209c828838
oHTGo <nguyennhathuy.orit@gmail.com>
2022-05-20 09:51:26 +0700

    Setup backend host frontend

---
+++ Api/Configurations/ConfigureControllerMapper.cs
@@ -0,0 +1,43 @@
+﻿namespace Api.Configurations
+{
+    public static class ConfigureControllerMapper
+    {
+        private static void AddMappingSpa(this WebApplication app)
+        {
+            app.MapWhen(ctx => !ctx.Request.Path.StartsWithSegments("/api"), config =>
+            {
+                if (app.Environment.IsDevelopment())
+                {
+                    config.UseSpa(spa =>
+                    {
+                        spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+                    });
+                    return;
+                }
+
+                config.UseStaticFiles();
+                config.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapFallbackToFile("index.html");
+                });
+            });
+        }
+
+        private static void AddMappingApi(this WebApplication app)
+        {
+            app.MapWhen(ctx => ctx.Request.Path.StartsWithSegments("/api"), config =>
+            {
+                config.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+            });
+        }
+
+        public static void AddControllerMapper(this WebApplication app)
+        {
+            app.AddMappingSpa();
+            app.AddMappingApi();
+        }
+    }
+}
