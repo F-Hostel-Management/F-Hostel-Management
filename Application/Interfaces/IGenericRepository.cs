06d7cc21f6e4045f9e27e472f9d2499c85ee447a
HuyB <huybui479@gmail.com>
2022-05-24 16:44:13 +0700

    Created FirstOrDefaultAsync

--- Application/Interfaces/IGenericRepository.cs
+++ Application/Interfaces/IGenericRepository.cs
@@ -9,14 +9,11 @@ using System.Threading.Tasks;
 namespace Application.Interfaces.IRepository;
 public interface IGenericRepository<T> where T : BaseEntity
 {
-    // c
     Task CreateAsync(T entity);
-    // r
     Task<List<T>> ListAsync();
     Task<T> FindByIdAsync(Guid id);
     Task<IList<T>> WhereAsync(Expression<Func<T, bool>> predicate, params string[] navigationProperties);
-    // u
     Task UpdateAsync(T updated);
-    // d
     Task<T> DeleteAsync(Guid id);
+    Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
 }
