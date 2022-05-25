64694689a9255ad1d381151f982c67c437b53a40
LeXuanDai0307 <lxdai0307@gmail.com>
2022-05-25 22:46:22 +0700

    Add SearchInput component

---
+++ Api/Client/src/components/Input/SearchInput/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+import { InputBase as Input } from '@mui/material'
+
+export const InputBase = styled(Input)`
+    & > input {
+        padding: 0;
+    }
+`
