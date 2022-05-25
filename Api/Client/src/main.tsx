0cf45cc36ee9f1c5419e53955624c51ddc6a6628
oHTGo <nguyennhathuy.orit@gmail.com>
2022-05-25 11:22:47 +0700

    Setup redux for frontend

--- Api/Client/src/main.tsx
+++ Api/Client/src/main.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 
+import GlobalStyles from './components/GlobalStyles'
 import { ThemeProvider } from '@mui/material'
 
 import App from './App'
-import GlobalStyles from './components/GlobalStyles'
+import { store } from './stores/reduxStore'
 import theme from './theme/'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <GlobalStyles>
-            <ThemeProvider theme={theme}>
-                <App />
-            </ThemeProvider>
-        </GlobalStyles>
+        <Provider store={store}>
+            <GlobalStyles>
+                <ThemeProvider theme={theme}>
+                    <App />
+                </ThemeProvider>
+            </GlobalStyles>
+        </Provider>
     </React.StrictMode>
 )
