584890d9ec48f27b9f12dadaaa99f20ee6b5f517
LeXuanDai0307 <lxdai0307@gmail.com>
2022-05-26 16:03:07 +0700

    Add linear-gradient to LinearProgress component

--- Api/Client/src/components/Progress/LinearProgress/index.tsx
+++ Api/Client/src/components/Progress/LinearProgress/index.tsx
@@ -1,31 +1,48 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import CircularProgress, {
-  circularProgressClasses,
-  CircularProgressProps,
-} from '@mui/material/CircularProgress';
-import { linearProgressClasses, LinearProgress as Progress } from '@mui/material';
+import React, { FC } from 'react'
 
-interface ILinearProgressProps {}
+import {
+    linearProgressClasses,
+    LinearProgress as Progress,
+} from '@mui/material'
+import { styled } from '@mui/material/styles'
 
-const BorderLinearProgress = styled(Progress)(({ theme }) => ({
-    height: 10,
-    borderRadius: 5,
-    [`&.${linearProgressClasses.colorPrimary}`]: {
-        backgroundColor:
-            theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
-    },
-    [`& .${linearProgressClasses.bar}`]: {
+import { LightenDarkenColor } from '../../../utils/LightenDarkenColor'
+
+interface ILinearProgressProps {
+    progress: number
+    bgrColor: string
+}
+
+const BorderLinearProgress = styled(Progress)<{ bgrColor: string }>(
+    ({ theme, bgrColor }) => ({
+        height: 12,
         borderRadius: 5,
-        backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
-    },
-}))
+        [`&.${linearProgressClasses.colorPrimary}`]: {
+            backgroundColor:
+                theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+        },
+        [`& .${linearProgressClasses.bar}`]: {
+            borderRadius: 5,
+            // backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+            background: `linear-gradient(to right, ${LightenDarkenColor(
+                bgrColor,
+                5
+            )}, ${LightenDarkenColor(
+                bgrColor,
+                80
+            )} ) !important`,
+        },
+    })
+)
 
-const LinearProgress: React.FunctionComponent<ILinearProgressProps> = (
-    props
-) => {
-    return <BorderLinearProgress variant="determinate" value={50} />
+const LinearProgress: FC<ILinearProgressProps> = ({ progress, bgrColor }) => {
+    return (
+        <BorderLinearProgress
+            bgrColor={bgrColor}
+            variant="determinate"
+            value={progress}
+        />
+    )
 }
 
 export default LinearProgress
