ad12887dabdc54c439745b41ef30d811459d6d39
LeXuanDai0307 <lxdai0307@gmail.com>
2022-05-27 16:25:38 +0700

    Add Greeting component in Home page

---
+++ Api/Client/src/pages/Home/components/Greeting/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    text-align: center;
+`
+export const Span = styled.span`
+    display: inline-block;
+    margin: 0 6px;
+`
