a4ebf5caec361aa3443cbbd38aee2925454e6db8
LeXuanDai0307 <lxdai0307@gmail.com>
2022-05-28 12:12:37 +0700

    Edit max-height CartShorten component

--- Api/Client/src/components/Card/CardShorten/styles.tsx
+++ Api/Client/src/components/Card/CardShorten/styles.tsx
@@ -7,5 +7,8 @@ export const CardMUI = styled(Card)`
     height: 100%;
     margin: 32px 0;
     box-shadow: 5px 3px 10px 0 rgb(21 15 15 / 30%) !important;
+
+    max-height: 500px;
+    overflow: hidden;
 `
 export const CardHeaderMUI = styled(CardHeader)``
