b3c00d2159f25f5a7007a4725a25aa6714753649
LeXuanDai0307 <lxdai0307@gmail.com>
2022-05-31 01:38:52 +0700

    Add enums

--- Api/Client/src/utils/enums.ts
+++ Api/Client/src/utils/enums.ts
@@ -1,5 +1,27 @@
-export enum Role {
+export enum IRole {
     TENANT_ROLE = 0,
     MANAGER_ROLE = 1,
     OWNER_ROLE = 2,
+}
+
+export enum IGender {
+    'Male',
+    'Female',
+    'Unknown',
+}
+
+export enum ECommitmentStatus {
+    'Closed',
+    'Pending',
+    'Active',
+}
+
+export enum ERoomType {
+    'Single',
+    'Double',
+}
+
+export enum ERoomStatus {
+    'Finished',
+    'Renting',
 }
