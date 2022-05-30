ace4fc43166a51d4f0ce0a0be5a6cffdf4331938
oHTGo <nguyennhathuy.orit@gmail.com>
2022-05-30 22:14:52 +0700

    Update OData

--- Api/Client/src/utils/ODataCaller.ts
+++ Api/Client/src/utils/ODataCaller.ts
@@ -1,13 +1,35 @@
-import { o, OdataQuery, OHandler } from 'odata'
+import axios, { AxiosResponse } from 'axios'
 import { HttpErrorToast } from './HttpErrorToast'
+import odataQuery, { ODataQuery } from 'odata-fluent-query'
 
-const baseURL = '/odata/'
-const handler: OHandler = o('')
+const token = localStorage.getItem('token')
+const instance = axios.create({
+    baseURL: '/odata',
+    responseType: 'json',
+    headers: {
+        Authorization: token ? `Bearer ${token}` : '',
+        'Content-Type': 'application/json',
+    },
+})
+
+instance.interceptors.response.use(undefined, (error) => {
+    const { status } = error.response
+    console.log(typeof status)
+    HttpErrorToast.show(status)
+})
+
+const responseBody = (response: AxiosResponse) => response?.data
+
+const sleep = (ms: number) => (response: AxiosResponse) =>
+    new Promise<AxiosResponse>((resolve) =>
+        setTimeout(() => resolve(response), ms)
+    )
 
 export const ODataCaller = {
-    get: (path: string, query?: OdataQuery) =>
-        handler
-            .get(baseURL + path)
-            .query(query)
-            .catch((err) => HttpErrorToast.show(err?.status)),
+    createBuilder: <T>() => odataQuery<T>(),
+    get: <T>(path: string, query: ODataQuery<T>) => {
+        const queryString = query.toString()
+        const url = path + '?' + queryString
+        return instance.get(url).then(sleep(1000)).then(responseBody)
+    },
 }
