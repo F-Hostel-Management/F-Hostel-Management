86e73456e3f22004e8b6d3abb9f9026b3dbaee92
DanielNg <dacnguyen9101@gmail.com>
2022-05-31 20:05:03 +0700

    issue

--- Api/Controllers/Rest/CommitmentsController.cs
+++ Api/Controllers/Rest/CommitmentsController.cs
@@ -67,14 +67,39 @@ public class CommitmentsController : BaseRestController
         return Ok(com);
     }
 
-/*    public async Task<IActionResult> OwnerApprovedCommitment([FromRoute] Guid RoomId)
-    {
-
-    }*/
-
     // owner conform commitment ==> com.status => approved
+    [HttpPatch("commitment/owner/status")]
+    public async Task<IActionResult> OwnerApprovedCommitment
+        ([FromRoute] Guid RoomId)
+    {
+        CommitmentEntity com = 
+            await _commitmentServices.GetPendingCommitmentByRoom(RoomId);
+        if (com == null)
+        {
+            return BadRequest();
+        }
+
+        await _commitmentServices.ApprovedCommitment(com);
+        return Ok(com);
+    }
+
+
 
     // tenant into commitment ==> com.status => done
+    [HttpPatch("commitment/tenant/status")]
+    public async Task<IActionResult> TenantDoneCommitment
+    ([FromRoute] Guid RoomId)
+    {
+        CommitmentEntity com =
+            await _commitmentServices.GetApprovedCommitmentByRoom(RoomId);
+        if (com == null)
+        {
+            return BadRequest();
+        }
 
-    // commitment expired ==> com.status => expired
+        await _commitmentServices.DoneCommitment(com);
+        return Ok(com);
+    }
+
+    // commitment expired ==> com.status => expired => remove all invoice schedules
 }
