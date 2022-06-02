5db304bd8846ea45800568494a1f9970563db380
HuyB <huybui479@gmail.com>
2022-06-02 10:54:59 +0700

    Required Field Validation

--- Api/UserFeatures/Requests/UploadIdentificationUserRequest.cs
+++ Api/UserFeatures/Requests/UploadIdentificationUserRequest.cs
@@ -1,12 +1,14 @@
 ﻿using Api.UserFeatures.CustomValidationAttributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace Api.UserFeatures.Requests
 {
     public class UploadIdentificationUserRequest
     {
+        [Required]
         [MaxFileSize(1024 * 1024 * 2)]
         public virtual IFormFile FrontIdentification { get; set; }
-
+        [Required]
         [MaxFileSize(1024 * 1024 * 2)]
         public virtual IFormFile BackIdentification { get; set; }
     }
