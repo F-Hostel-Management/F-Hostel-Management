419a4e964ec030f9b5aff95c0cc8969da36a039f
HuyB <huybui479@gmail.com>
2022-06-02 10:51:26 +0700

    Created migration of ID card

---
+++ Persistence/Migrations/20220602035059_AddFrontBackIDCard.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddFrontBackIDCard : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Identification",
+                table: "Users",
+                newName: "FrontIdentification");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BackIdentification",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BackIdentification",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "FrontIdentification",
+                table: "Users",
+                newName: "Identification");
+        }
+    }
+}
