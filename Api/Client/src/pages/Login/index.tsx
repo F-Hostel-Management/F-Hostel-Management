4da19c56651f02bbec6c68ab98b3b9439134db6b
oHTGo <nguyennhathuy.orit@gmail.com>
2022-06-03 22:18:46 +0700

    Update dispatch in Login page

--- Api/Client/src/pages/Login/index.tsx
+++ Api/Client/src/pages/Login/index.tsx
@@ -10,6 +10,9 @@ import MicrosoftLogo from '../../assets/images/MicrosoftLogo.svg'
 import FirebaseService from '../../services/FirebaseService'
 import { RestCaller } from '../../utils/RestCaller'
 
+import { useDispatch } from 'react-redux'
+import { setIsAuthenticated } from '../../slices/authSlice'
+
 interface ILoginProps {}
 
 interface IExchangeTokenResponse {
@@ -19,6 +22,7 @@ interface IExchangeTokenResponse {
 
 const Login: React.FunctionComponent<ILoginProps> = () => {
     const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     const doLogin = async (firebaseToken: string) => {
         const result = await exchangeToken(firebaseToken)
@@ -27,7 +31,7 @@ const Login: React.FunctionComponent<ILoginProps> = () => {
         const { isFirstTime } = result
         if (isFirstTime) return redirectFirstTimePage()
 
-        redirectHomePage()
+        dispatch(setIsAuthenticated(true))
     }
 
     const exchangeToken = async (
@@ -44,10 +48,6 @@ const Login: React.FunctionComponent<ILoginProps> = () => {
 
     const redirectFirstTimePage = () => {
         navigate('/fillInformation')
-    }
-
-    const redirectHomePage = () => {
-        navigate('/home')
     }
 
     const onSignInGoogle = async () => {
