52109573a1648b3487ab419cfc8c831178c07e71
oHTGo <nguyennhathuy.orit@gmail.com>
2022-06-03 22:19:20 +0700

    Update state isAuthenticated in routers

--- Api/Client/src/routes/PrivateRouter.tsx
+++ Api/Client/src/routes/PrivateRouter.tsx
@@ -1,11 +1,32 @@
-import * as React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { Navigate, Outlet } from 'react-router-dom'
+import { IUser } from '../interface/user'
+import { setCurrentUser } from '../slices/authSlice'
+import { AppState } from '../stores/reduxStore'
+import { RestCaller } from '../utils/RestCaller'
 
 interface IPrivateRouteProps {}
 
 const PrivateRoute: React.FunctionComponent<IPrivateRouteProps> = () => {
-    let isAuthenticated = true
+    const isAuthenticated = useSelector(
+        (state: AppState) => state.auth.isAuthenticated
+    )
+    const dispatch = useDispatch()
+    useEffect(() => {
+        const checkAuthentication = async () => {
+            if (isAuthenticated) return
+
+            const response = await RestCaller.get('Users/info')
+            if (!response || response.isError) return
+
+            const result: IUser = response.result
+            dispatch(setCurrentUser(result))
+        }
+
+        checkAuthentication()
+    }, [])
     return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
 }
 
