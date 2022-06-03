298835ec9f882ae81fe07761e046794b7f64bdb9
oHTGo <nguyennhathuy.orit@gmail.com>
2022-06-03 22:18:09 +0700

    Update Auth slice

--- Api/Client/src/slices/authSlice.ts
+++ Api/Client/src/slices/authSlice.ts
@@ -1,7 +1,9 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { IUser } from '../interface/user'
 
 interface AuthSliceState {
     isAuthenticated: boolean
+    currentUser?: IUser
 }
 
 const initialState: AuthSliceState = {
@@ -11,10 +13,24 @@ const initialState: AuthSliceState = {
 const authSlice = createSlice({
     name: 'auth',
     initialState,
-    reducers: {},
+    reducers: {
+        setCurrentUser: (
+            state: AuthSliceState,
+            actions: PayloadAction<IUser>
+        ) => {
+            state.currentUser = actions.payload
+            state.isAuthenticated = true
+        },
+        setIsAuthenticated: (
+            state: AuthSliceState,
+            actions: PayloadAction<boolean>
+        ) => {
+            state.isAuthenticated = actions.payload
+        },
+    },
 })
 
-export const {} = authSlice.actions
+export const { setCurrentUser, setIsAuthenticated } = authSlice.actions
 
 const { reducer } = authSlice
 export default reducer
