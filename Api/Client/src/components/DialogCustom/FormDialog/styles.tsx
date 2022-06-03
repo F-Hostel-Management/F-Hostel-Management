6b093f8cd26456a89d6967aaacf99ffab8b827e9
LeXuanDai0307 <lxdai0307@gmail.com>
2022-06-04 00:18:11 +0700

    Add FormDialog component

---
+++ Api/Client/src/components/DialogCustom/FormDialog/styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const GroupButton = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    & button {
+        margin: 8px;
+    }
+`
