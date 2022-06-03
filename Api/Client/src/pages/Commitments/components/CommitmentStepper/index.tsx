c36f7fd0964e1cb2cc087bdbc978ea3b0bb11628
LeXuanDai0307 <lxdai0307@gmail.com>
2022-06-03 10:26:08 +0700

    Edit create commitment StepByStep dialog

--- Api/Client/src/pages/Commitments/components/CommitmentStepper/index.tsx
+++ Api/Client/src/pages/Commitments/components/CommitmentStepper/index.tsx
@@ -1,20 +1,9 @@
-import React, {
-    FormEvent,
-    FC,
-    ChangeEvent,
-    Dispatch,
-    SetStateAction,
-} from 'react'
-import Box from '@mui/material/Box'
-import Stepper from '@mui/material/Stepper'
-import Step from '@mui/material/Step'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
-import { StepButton } from '@mui/material'
+import React, { FC, ChangeEvent, Dispatch, SetStateAction } from 'react'
 import Step1 from './Step1'
 import Step2 from './Step2'
 import Step3 from './Step3'
 import { rooms } from '../../../../utils/MockData'
+import StepByStep from '../../../../components/StepByStep'
 
 interface ICommitmentStepperProps {
     handleCloseDialog: () => void
@@ -39,11 +28,6 @@ const CommitmentStepper: FC<ICommitmentStepperProps> = ({
     hostelInfo,
     setHostelInfo,
 }) => {
-    const [activeStep, setActiveStep] = React.useState(0)
-    const [completed, setCompleted] = React.useState<{
-        [k: number]: boolean
-    }>({})
-
     const steps = [
         {
             name: 'Terms',
@@ -79,107 +63,7 @@ const CommitmentStepper: FC<ICommitmentStepperProps> = ({
         },
     ]
 
-    const totalSteps = () => {
-        return steps.length
-    }
-
-    const allStepsCompleted = () => {
-        return activeStep === totalSteps()
-    }
-
-    const handleStep = (step: number) => () => {
-        setActiveStep(step)
-    }
-
-    const handleNext = () => {
-        setActiveStep(activeStep + 1)
-        const newCompleted = completed
-        newCompleted[activeStep] = true
-        setCompleted(newCompleted)
-    }
-
-    const handleReset = () => {
-        setActiveStep(0)
-        handleCloseDialog()
-        resetForm()
-    }
-
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        console.log('Submit Data: ', values)
-    }
-    return (
-        <Box sx={{ width: '100%' }}>
-            <Stepper
-                nonLinear
-                activeStep={activeStep}
-                sx={{ width: '80%', margin: 'auto' }}
-            >
-                {steps.map((step, index) => (
-                    <Step key={step?.name} completed={completed[index]}>
-                        <StepButton color="inherit" onClick={handleStep(index)}>
-                            {step?.name}
-                        </StepButton>
-                    </Step>
-                ))}
-            </Stepper>
-            <div>
-                {allStepsCompleted() ? (
-                    <React.Fragment>
-                        <Typography
-                            sx={{
-                                mt: 2,
-                                mb: 1,
-                                minHeight: '300px',
-                                textAlign: 'center',
-                            }}
-                        >
-                            All steps completed - you&apos;re finished
-                        </Typography>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                pt: 2,
-                            }}
-                        >
-                            <Box sx={{ flex: '1 1 auto' }} />
-                            <Button variant="outlined" onClick={handleReset}>
-                                Close
-                            </Button>
-                        </Box>
-                    </React.Fragment>
-                ) : (
-                    <form onSubmit={handleSubmit}>
-                        <React.Fragment>
-                            {steps[activeStep]?.component}
-                        </React.Fragment>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                pt: 2,
-                            }}
-                        >
-                            <Box sx={{ flex: '1 1 auto' }} />
-                            {activeStep !== steps.length && (
-                                <Button
-                                    variant="contained"
-                                    size="small"
-                                    onClick={handleNext}
-                                    type="submit"
-                                >
-                                    {activeStep === totalSteps() - 1
-                                        ? 'Confirm'
-                                        : 'Next'}
-                                </Button>
-                            )}
-                        </Box>
-                    </form>
-                )}
-            </div>
-        </Box>
-    )
+    return <StepByStep steps={steps} handleCloseDialog={handleCloseDialog} />
 }
 
 export default CommitmentStepper
