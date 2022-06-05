b1eacd39435cd9a325ea3e531eabce35f51c3e76
LeXuanDai0307 <lxdai0307@gmail.com>
2022-06-05 14:01:40 +0700

    Edit HostelForm in Homepage

--- Api/Client/src/pages/Home/components/HostelForm/index.tsx
+++ Api/Client/src/pages/Home/components/HostelForm/index.tsx
@@ -1,5 +1,4 @@
-import { Button, Card, CardMedia } from '@mui/material'
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
 import InputField from '../../../../components/Input/InputField'
 import { IField } from '../../../../interface/IField'
 import * as Styled from './styles'
@@ -7,7 +6,6 @@ import { styled } from '@mui/material/styles'
 const Input = styled('input')({
     display: 'none',
 })
-import PhotoCameraIcon from '@mui/icons-material/PhotoCamera'
 
 interface IHostelFormProps {
     values: any
@@ -41,57 +39,22 @@ const HostelForm: FC<IHostelFormProps> = ({
         },
     ]
 
-    const [image, setImage] = useState<string>()
-
-    const handleChooseImageHostel = (e: any) => {
-        setImage(URL.createObjectURL(e.target.files[0]))
-    }
     return (
-        <Styled.ContainerStep>
-            <Styled.LeftSide>
-                <Card sx={{ height: '300', width: '100%' }}>
-                    <CardMedia
-                        component="img"
-                        height="300"
-                        image={image}
-                        alt="Hostel Image"
-                    />
-                </Card>
-                <Styled.UploadButton htmlFor="contained-button-file">
-                    <Input
-                        accept="image/*"
-                        id="contained-button-file"
-                        multiple={false}
-                        type="file"
-                        onChange={handleChooseImageHostel}
-                    />
-                    <Button
-                        variant="contained"
-                        color="orange"
-                        component="span"
-                        size="small"
-                        startIcon={<PhotoCameraIcon />}
-                    >
-                        Upload
-                    </Button>
-                </Styled.UploadButton>
-            </Styled.LeftSide>
-            <Styled.RightSide>
-                {fields.slice(0, 3).map((field) => (
-                    <InputField
-                        key={field.name}
-                        label={field.label}
-                        name={field.name}
-                        value={values[field.name]}
-                        type={field.type}
-                        required={field.required}
-                        disabled={field.disabled}
-                        endAdornment={field.endAdornment}
-                        onChange={handleInputChange}
-                    />
-                ))}
-            </Styled.RightSide>
-        </Styled.ContainerStep>
+        <Styled.Wrapper>
+            {fields.slice(0, 3).map((field) => (
+                <InputField
+                    key={field.name}
+                    label={field.label}
+                    name={field.name}
+                    value={values[field.name]}
+                    type={field.type}
+                    required={field.required}
+                    disabled={field.disabled}
+                    endAdornment={field.endAdornment}
+                    onChange={handleInputChange}
+                />
+            ))}
+        </Styled.Wrapper>
     )
 }
 
--- Api/Client/src/pages/Home/components/HostelForm/styles.tsx
+++ Api/Client/src/pages/Home/components/HostelForm/styles.tsx
@@ -1,28 +1,12 @@
-import { down } from 'styled-breakpoints'
 import styled from 'styled-components'
 
-export const ContainerStep = styled.div`
-    position: relative;
-    width: 100%;
-    min-height: 320px;
-    margin: 16px;
-    display: flex;
-
-    ${down('md')} {
-        flex-direction: column;
-    }
-`
-const Wrapper = styled.div`
+export const Wrapper = styled.div`
     flex: 1;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-`
-export const LeftSide = styled(Wrapper)`
-    border: 1px dashed #000;
-`
-export const RightSide = styled(Wrapper)``
-export const UploadButton = styled.label`
-    position: absolute;
+
+    min-height: 300px;
+    margin: 16px;
 `
