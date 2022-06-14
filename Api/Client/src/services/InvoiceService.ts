f93198f287d49aa3842fc27aac0f297e879dd615
oHTGo <nguyennhathuy.orit@gmail.com>
2022-06-14 14:22:18 +0700

    Fix get invoice by hostel

--- Api/Client/src/services/InvoiceService.ts
+++ Api/Client/src/services/InvoiceService.ts
@@ -1,4 +1,5 @@
 import { IInvoice } from '../interface/IInvoice'
+import { getItem } from '../utils/LocalStorageUtils'
 import { ODataCaller } from '../utils/ODataCaller'
 import { RestCaller } from '../utils/RestCaller'
 
@@ -14,6 +15,7 @@ export const getInvoices = async (
     pageSize: number,
     page: number
 ): Promise<IInvoice[]> => {
+    const currentHostelId = getItem('currentHostelId')
     const builder = createBuilder<IInvoice>()
         .select(
             'id',
@@ -24,13 +26,15 @@ export const getInvoices = async (
             'content',
             'invoiceType'
         )
-        .expand('room', (q) => q.select('id', 'roomName'))
+        .expand('room', (q) => q.select('id', 'roomName', 'hostelId'))
         .expand('manager', (q) => q.select('id', 'name'))
         .expand('tenantPaid', (q) => q.select('id', 'name'))
         .paginate(pageSize, page)
-    const result = await get('Invoices', builder)
-    console.log(result)
-    return result
+    const result: IInvoice[] = await get('Invoices', builder)
+
+    return result.filter(
+        (invoice) => invoice.room?.hostelId === currentHostelId
+    )
 }
 
 interface ICreateInvoiceParams {
