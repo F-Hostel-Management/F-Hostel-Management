59272bfa2a872109ade901ff4e2d9a7b8e3c04e7
LeXuanDai0307 <lxdai0307@gmail.com>
2022-06-20 16:04:07 +0700

    Add ComboBox generic component

--- Api/Client/src/components/ComboBox/index.tsx
+++ Api/Client/src/components/ComboBox/index.tsx
@@ -2,34 +2,39 @@ import { Autocomplete, Paper } from '@mui/material'
 import React, { FC } from 'react'
 import InputField from '../Input/InputField'
 
-interface IComboBoxProps {
+interface IComboBoxProps<T> {
     label: string
-    options: Record<string, any>[]
     optionLabel: string
-    valueAutocomplete: any
-    setValueAutocomplete: any
+    options: T[]
+    valueAutocomplete: T
+    setValueAutocomplete: (valueAutocomplete: T) => void
     [x: string | number | symbol]: unknown
 }
 
-const ComboBox: FC<IComboBoxProps> = ({
+const ComboBox = <T extends Record<string, any>>({
     label,
     optionLabel,
     options,
     valueAutocomplete,
     setValueAutocomplete,
     ...others
-}) => {
+}: IComboBoxProps<T>) => {
     const [inputValue, setInputValue] = React.useState('')
+
     const CustomPaper: FC<{}> = (props) => {
         return <Paper elevation={8} style={{ fontSize: '1.6rem' }} {...props} />
     }
+
     return (
         <Autocomplete
             disablePortal
             id="combo-box"
             value={valueAutocomplete}
-            onChange={(event: any, newValue: string | null) => {
-                setValueAutocomplete(newValue)
+            onChange={(
+                event: React.SyntheticEvent<Element, Event>,
+                newValue: T
+            ) => {
+                setValueAutocomplete(newValue || null)
             }}
             inputValue={inputValue}
             onInputChange={(event, newInputValue) => {
--- Api/Client/src/components/Layout/DefaultLayout/index.tsx
+++ Api/Client/src/components/Layout/DefaultLayout/index.tsx
@@ -38,17 +38,26 @@ const DefaultLayout: FC<IDefaultLayoutProps> = ({ title, children }) => {
     const [isSidebarMobile, setIsSidebarMobile] = useState<boolean>(false)
 
     useEffect(() => {
-        if (role === ERole.TENANT_ROLE) return
-        const hostelId = localStorage.getItem('currentHostelId')
-        if (!hostelId) {
-            navigate('/home')
-            return
-        } else if (!Object.keys(currentHostel).length) {
-            ;(async () => {
-                const hostel = await getHostelById(hostelId)
-                dispatch(setCurrentHostel(hostel))
-            })()
-        } else return
+        if (role === ERole.TENANT_ROLE) {
+            const roomId = localStorage.getItem('currentRoomId')
+            if (!roomId) {
+                navigate('/home')
+                return
+            }
+        }
+        if (role === ERole.OWNER_ROLE || role === ERole.MANAGER_ROLE) {
+            const hostelId = localStorage.getItem('currentHostelId')
+            if (!hostelId) {
+                navigate('/home')
+                return
+            } else if (!Object.keys(currentHostel).length) {
+                ;(async () => {
+                    const hostel = await getHostelById(hostelId)
+                    dispatch(setCurrentHostel(hostel))
+                })()
+            }
+        }
+        return
     }, [currentHostel])
 
     return (
