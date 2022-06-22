2053e6a178d80fb63881450f3a135d72c49756fe
danielNG <dacnguyen9101@gmail.com>
2022-06-22 09:31:12 +0700

    Fix logic database

---
+++ Persistence/Migrations/20220622022811_RemoveOwnerIdInUsers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class RemoveOwnerIdInUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Users_OwnerId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_OwnerId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_OwnerId",
+                table: "Users",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Users_OwnerId",
+                table: "Users",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
