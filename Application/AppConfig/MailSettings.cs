03c981c265e4f811102ac89e120c36d2b33786b3
HuyB <huybui479@gmail.com>
2022-06-22 08:03:54 +0700

    use sendgrid email providor

--- Application/AppConfig/MailSettings.cs
+++ Application/AppConfig/MailSettings.cs
@@ -4,6 +4,7 @@ public class MailSettings
 {
     public string Mail { get; set; }
     public string DisplayName { get; set; }
+    public string UserName { get; set; }
     public string Password { get; set; }
     public string Host { get; set; }
     public int Port { get; set; }
