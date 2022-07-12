e55e7560139facdde5767722aaaba5e272faf84a
oHTGo <nguyennhathuy.orit@gmail.com>
2022-07-13 00:24:04 +0700

    Remove implicit import

--- Application/Interfaces/ITokenService.cs
+++ Application/Interfaces/ITokenService.cs
@@ -1,17 +1,12 @@
 ﻿using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 using Application.Models;
 
 namespace Application.Interfaces;
 
 public interface ITokenService
 {
-   string GetToken(UserEntity user);
-   string GenerateAssignmentToken(AssignmentPayload assignmentPayload);
-   IEnumerable<Claim> DecodeAndValidateToken(string token);
+    string GetToken(UserEntity user);
+    string GenerateAssignmentToken(AssignmentPayload assignmentPayload);
+    IEnumerable<Claim> DecodeAndValidateToken(string token);
 }
