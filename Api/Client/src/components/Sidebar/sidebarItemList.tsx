649f427b55d51a857f10984b0f90b2d2c9954c2f
HuyB <huybui479@gmail.com>
2022-07-31 19:18:39 +0700

    update sidebar for manager

--- Api/Client/src/components/Sidebar/sidebarItemList.tsx
+++ Api/Client/src/components/Sidebar/sidebarItemList.tsx
@@ -82,7 +82,54 @@ export const sidebarItemList: {
     manager: [
         {
             groupLabel: '--MAIN',
-            items: [],
+            items: [
+                {
+                    icon: <Icon name="dashboard" />,
+                    label: 'Dashboard',
+                    path: '/home/dashboard',
+                },
+                {
+                    icon: <Icon name="room" />,
+                    label: 'Rooms',
+                    path: '/home/rooms',
+                },
+                {
+                    icon: <Icon name="tenant" />,
+                    label: 'Tenants',
+                    path: '/home/tenants',
+                },
+
+                // {
+                //     icon: <Icon name="notification" />,
+                //     label: 'Notifications',
+                //     path: '/home/notifications',
+                // },
+                // {
+                //     icon: <Icon name="ticket" />,
+                //     label: 'Tickets',
+                //     path: '/home/tickets',
+                // },
+                {
+                    icon: <Icon name="schedulingInvoice" />,
+                    label: 'Scheduling Invoices',
+                    path: '/home/schedulingInvoices',
+                },
+                {
+                    icon: <Icon name="invoice" />,
+                    label: 'Invoices',
+                    path: '/home/invoices',
+                },
+                {
+                    icon: <Icon name="commitment" />,
+                    label: 'Commitments',
+                    path: '/home/commitments',
+                },
+                {
+                    icon: <Icon name="facility" />,
+                    label: 'Facilities',
+                    path: '/home/facilities',
+                },
+            ],
         },
     ],
     tenant: [
@@ -91,7 +138,7 @@ export const sidebarItemList: {
             items: [
                 {
                     icon: <Icon name="room" />,
-                    label: 'Rooms',
+                    label: 'My Room',
                     path: '/home/rooms',
                 },
                 {
